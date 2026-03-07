6518ba02e5d2f128c133396dd76e5d55b89be869
Bourne Young <young40@gmail.com>
2026-03-07 22:43:58 +0800

    feat: add scene transition and display configuration
    - Add scene transition from main to menu scene
    - Add ChangeSceneToPacked method for scene management
    - Update display configuration with viewport and stretch settings

--- main/Main.cs
+++ main/Main.cs
@@ -7,10 +7,32 @@ public partial class Main : Node
 	public override void _Ready()
 	{
 		GD.Print("Hello Godot!");
+		
+		GoToMainMenu();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void GoToMainMenu()
+	{
+		PackedScene menu = ResourceLoader.Load<PackedScene>("res://menu/menu.tscn");
+		
+		ChangeSceneToPacked(menu);
+	}
+
+	private void ChangeSceneToPacked(PackedScene scene)
+	{
+		foreach (var child in GetChildren())
+		{
+			RemoveChild(child);	
+			child.QueueFree();
+		}
+
+		Node node = scene.Instantiate();
+
+		AddChild(node);
 	}
 }
