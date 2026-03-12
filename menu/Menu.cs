c2c896eb07c61b3f58b84cd1e0a76adba006672a
Bourne Young <young40@gmail.com>
2026-03-13 00:50:01 +0800

    feat(menu): add level loading screen with progress bar and timer

--- menu/Menu.cs
+++ menu/Menu.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Threading.Tasks;
+using Array = Godot.Collections.Array;
 
 public partial class Menu : Node
 {
@@ -8,6 +9,11 @@ public partial class Menu : Node
 
 	[Export] private CanvasItem _nodeMain;
 	[Export] private CanvasItem _nodeLoading;
+
+	[Export] private ProgressBar _nodeLoadingProgressBar;
+	[Export] private Timer _nodeLoadingDoneTimer;
+
+	private const string LEVE_PATH = "res://level/level.tscn";
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -21,6 +27,32 @@ public partial class Menu : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (_nodeLoading.Visible)
+		{
+			Godot.Collections.Array progress = new Array();
+			ResourceLoader.ThreadLoadStatus status = ResourceLoader.LoadThreadedGetStatus(LEVE_PATH, progress);
+
+			if (status == ResourceLoader.ThreadLoadStatus.InProgress)
+			{
+				_nodeLoadingProgressBar.Value = progress[0].AsDouble() * 100f;
+			}
+			else if (status == ResourceLoader.ThreadLoadStatus.Loaded)
+			{
+				GD.Print("New level loaded");
+				
+				_nodeLoadingProgressBar.Value = 100f;	
+				SetProcess(false);
+				
+				_nodeLoadingDoneTimer.Start();
+			}
+			else
+			{
+				GD.PrintErr("Error while loading level: " + status);
+				_nodeLoading.Hide();
+				_nodeMain.Show();
+			}
+
+		}
 	}
 
 	private void OnPlayPressed()
@@ -29,11 +61,18 @@ public partial class Menu : Node
 		
 		_nodeMain.Hide();
 		_nodeLoading.Show();
+		
+		ResourceLoader.LoadThreadedRequest(LEVE_PATH, "", true);
 	}
 	
 	private void OnQuitPressed()
 	{
 		GD.PrintErr("Quit pressed");
 		GetTree().Quit();
+	}
+
+	private void OnLoadingDoneTimerEnded()
+	{
+		GD.PrintErr("Loading done");
 	}
 }
